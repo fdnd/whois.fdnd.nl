1a879a0020ad8f558b1b868b1114f1be3ebc40ea
ju5tu5 <justus@ju5tu5.nl>
2023-03-17 00:57:55 +0100

    Added post route

--- src/routes/api/v1/member/+server.js
+++ src/routes/api/v1/member/+server.js
@@ -1,9 +1,10 @@
 import { GraphQLClient, gql } from 'graphql-request';
-import { HYGRAPH_KEY, HYGRAPH_URL_HIGH_PERFORMANCE } from '$env/static/private';
+import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private';
 
+import { htmlToSlateAST } from '@graphcms/html-to-slate-ast';
 import { responseInit } from '$lib/server/responseInit';
 
-const hygraph = new GraphQLClient(HYGRAPH_URL_HIGH_PERFORMANCE, {
+const hygraph = new GraphQLClient(HYGRAPH_URL, {
 	headers: { Authorization: `Bearer ${HYGRAPH_KEY}` }
 });
 
@@ -48,9 +49,70 @@ export async function GET({ url }) {
 }
 
 export async function POST({ request }) {
-	const request_data = await request.json();
-	let value = request_data.value;
-	return new Response(JSON.stringify({ method: 'POST', working: 'no' }), responseInit);
+	const formdata = await request.json();
+	// Voor rich text velden moet de ruwe data omgerekend worden
+	formdata.bio = await htmlToSlateAST(fdata.bio);
+
+	const mutation = gql`
+		mutation createMember(
+			$slug: String!
+			$name: String!
+			$prefix: String!
+			$surname: String!
+			$nickname: String!
+			$avatar: String!
+			$gitHubHandle: String!
+			$bio: RichTextAST!
+			$website: String!
+			$colour: Hex!
+			$squad: ID!
+		) {
+			createMember(
+				data: {
+					slug: $slug
+					name: $name
+					prefix: $prefix
+					surname: $surname
+					nickname: $nickname
+					avatar: $avatar
+					gitHubHandle: $gitHubHandle
+					bio: { children: $bio }
+					website: $website
+					colour: { hex: $colour }
+					squads: { connect: { id: $squad } }
+				}
+			) {
+				id
+			}
+		}
+	`;
+
+	const data = await hygraph.request(mutation, { ...formdata }).catch(error => {
+		return new Response(error, responseInit);
+	});
+
+	const publication = gql`
+		mutation publishMember($id: ID!) {
+			publishMember(where: { id: $id }, to: PUBLISHED) {
+				id
+			}
+		}
+	`;
+
+	const pubData = await hygraph.request(publication, { id: data.createMember.id }).catch(error => {
+		return new Response(error, responseInit);
+	});
+
+	return new Response(
+		JSON.stringify({
+			method: 'POST',
+			working: 'yes',
+			success: pubData.publishMember !== undefined,
+			data: data,
+			pubData: pubData
+		}),
+		responseInit
+	);
 }
 
 export async function PUT({ request }) {
